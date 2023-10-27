97120e9811487b796ff92dd0ad91aab8fd2419ad
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2023-10-27 13:25:55 -0300

    Add the logo and navegations elements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,34 @@
+import Logo from '../assets/brand/Logo.svg'
+
+const MenuBar = (props) => {
+    return  (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    );
+    
+}
+
+const Logotype = (props) => {
+    return (
+        <img src={Logo} alt="logo" />
+    )
+}
+
 const Nav = (props) => {
-    return <nav>nav</nav>
+    return (
+        <>
+            <Logotype />
+            <MenuBar />
+        </>
+    )
 }
 
 export { Nav };
