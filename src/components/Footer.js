c87a387cbd94f4c51c5c4d08df37f77d576e07b8
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2023-10-27 14:20:44 -0300

    Add the footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,59 @@
+const FooterImg = (props) => {
+    return <img src="#" alt="Logo vertical" />
+}
+
+
+const VerticalNavigation = (props) => {
+    return (
+        <div>
+            <h3>Doormat Navigation</h3>
+            <ul>
+                <li>Home</li>
+                <li>about</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}
+
+const VerticalNavigationContact = (props) => {
+    return (
+        <div>
+            <h3>Contact</h3>
+            <ul>
+                <li>Adress</li>
+                <li>Phone number</li>
+                <li>Email</li>
+            </ul>
+        </div>
+    )
+}
+
+const VerticalNavigationSocial = (props) => {
+    return (
+        <div>
+            <h3>Contact</h3>
+            <ul>
+                <li>Adress</li>
+                <li>Phone number</li>
+                <li>Email</li>
+            </ul>
+        </div>
+    )
+}
+
 const Footer = () => {
-    return <footer>footer</footer>
+    return (
+        <>
+            <FooterImg />
+            <VerticalNavigation />
+            <VerticalNavigationContact />
+            <VerticalNavigationSocial />
+        </>
+    )
 }
 
 export { Footer };
