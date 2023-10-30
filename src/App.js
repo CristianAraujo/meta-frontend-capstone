d90656946dd650bb2860dca6b9b806521d71f44a
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2023-10-30 11:39:44 -0300

    Component About imported in App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
 import { Testimonials } from './components/Testimonials';
+import { About } from './components/About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header  />
       <Main className="main" />
       <Testimonials />
+      <About />
       <Footer className="footer" />
     </>
   );
